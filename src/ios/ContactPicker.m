abc86400c855adb151cb5208a80942ed70c1d5b6
Nishil Shah <nishil.shah17@gmail.com>
2014-07-15 09:31:56 -0400

    people picker navigation controller

--- src/ios/ContactPicker.m
+++ src/ios/ContactPicker.m
@@ -17,3 +17,31 @@
     [super writeJavascript:[[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Canceled Person Picker"]
       toErrorCallbackString:self.callbackID]];
 }
+
+- (BOOL)peoplePickerNavigationController:(ABPeoplePickerNavigationController*)peoplePicker
+      shouldContinueAfterSelectingPerson:(ABRecordRef)person
+                                property:(ABPropertyID)property
+                              identifier:(ABMultiValueIdentifier)identifier
+{
+  ABMultiValueRef multi = ABRecordCopyValue(person, kABPersonEmailProperty);
+  int index = ABMultiValueGetIndexForIdentifier(multi, identifier);
+  NSString *email = (__bridge NSString *)ABMultiValueCopyValueAtIndex(multi, index);
+  NSString *displayName = (__bridge NSString *)ABRecordCopyCompositeName(person);
+  ABMultiValueRef multiPhones = ABRecordCopyValue(person, kABPersonPhoneProperty);
+  NSString* phoneNumber = @"";
+  for(CFIndex i = 0; i < ABMultiValueGetCount(multiPhones); i++) {
+      if(identifier == ABMultiValueGetIdentifierAtIndex (multiPhones, i)) {
+          phoneNumber = (__bridge NSString *)ABMultiValueCopyValueAtIndex(multiPhones, i);
+          break;
+      }
+  }
+
+  NSMutableDictionary* contact = [NSMutableDictionary dictionaryWithCapacity:2];
+  [contact setObject:email forKey: @"email"];
+  [contact setObject:displayName forKey: @"displayName"];
+  [contact setObject:phoneNumber forKey: @"phoneNumber"];
+
+  [super writeJavascript:[[CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:contact] toSuccessCallbackString:self.callbackID]];
+  [self.viewController dismissModalViewControllerAnimated:YES];
+  return NO;
+}
