f1063eaadfa62c2796e7114dc4551c326e1027ea
Rohith Varanasi <Rohvar98@gmail.com>
2014-07-15 14:46:17 -0400

    redid the js, should work now.

--- www/ContactPicker.js
+++ www/ContactPicker.js
@@ -1,15 +1,23 @@
+cordova.define("com.monmouth.ContactPicker", function(require, exports, module) { /**
+ * @constructor
+ */
 var ContactPicker = function(){};
 
-window.pickContact = function(success, failure, field, defaultAction) {
-    cordova.exec(success, failure, "ContactPicker", "pickContact", [field, defaultAction]);
+
+ContactPicker.prototype.pickContact = function(success, failure, field){
+    cordova.exec(success, failure, "ContactPicker", "pickContact", [field]);
 };
 
+// Plug in to Cordova
 cordova.addConstructor(function() {
 
     if (!window.Cordova) {
         window.Cordova = cordova;
     };
 
+
     if(!window.plugins) window.plugins = {};
     window.plugins.ContactPicker = new ContactPicker();
+});
+
 });
