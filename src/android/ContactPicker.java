4a201dba7f14b317e2611efa3e09777cda2fd0f9
Rohith Varanasi <Rohvar98@gmail.com>
2014-07-15 11:58:51 -0400

    added OnClickListener import

--- src/android/ContactPicker.java
+++ src/android/ContactPicker.java
@@ -6,6 +6,7 @@ import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
